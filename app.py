63a5b15864b54c20981a0ed9e03049b651f953f7
shindeal <97160971+shindeal@users.noreply.github.com>
2024-07-22 16:14:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,19 @@ import pyodbc
 import pandas as pd
 
 # Initialize connection.
-# Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
     connection_string = (
         "DRIVER={ODBC Driver 17 for SQL Server};"
         "SERVER=" + st.secrets["server"] + ";"
         "DATABASE=" + st.secrets["database"] + ";"
-        "Trusted_Connection=yes;"
+        "Trusted_Connection=yes;"  # Use this for Windows Authentication
     )
     return pyodbc.connect(connection_string)
 
 conn = init_connection()
 
 # Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
