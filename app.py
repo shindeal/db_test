f5d7a5aa2e2802fb7740155448d36d270b548b2b
shindeal <97160971+shindeal@users.noreply.github.com>
2024-07-22 16:09:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 import streamlit as st
+import pyodbc
 import pandas as pd
-from sqlalchemy import create_engine
-import urllib
 
-# Database connection details
-server = "L1SQLS1601P\\SpeedyDWAnalytic"
-database = "Speedy_Models"
-driver = "ODBC Driver 17 for SQL Server"
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    connection_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=" + st.secrets["server"] + ";"
+        "DATABASE=" + st.secrets["database"] + ";"
+        "Trusted_Connection=yes;"
+    )
+    return pyodbc.connect(connection_string)
 
-# Create a connection string
-params = urllib.parse.quote_plus(f"DRIVER={{{driver}}};SERVER={server};DATABASE={database};Trusted_Connection=yes;")
-connection_string = f"mssql+pyodbc:///?odbc_connect={params}"
+conn = init_connection()
 
-# Function to establish a database connection and retrieve data
-@st.cache_data
-def get_data(query):
-    engine = create_engine(connection_string)
-    with engine.connect() as conn:
-        data = pd.read_sql(query, conn)
-    return data
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        columns = [column[0] for column in cur.description]
+        data = cur.fetchall()
+        return columns, data
 
-# Streamlit app
-def main():
-    st.title("SQL Server Data Viewer")
+query = "SELECT TOP 10 * FROM dim_depot with (nolock);"
 
-    # SQL query to execute
-    query = "SELECT TOP 10 * FROM dim_depot with (nolock)"  # Adjust the query as needed
+# Retrieve data from the database
+columns, rows = run_query(query)
 
-    # Retrieve data from the database
-    data = get_data(query)
-
-    # Display the data in Streamlit
-    st.write(data)
-
-if __name__ == "__main__":
-    main()
+# Print results.
+st.write("### Query Results")
+df = pd.DataFrame(rows, columns=columns)
+st.write(df)
