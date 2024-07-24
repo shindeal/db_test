a06cf90050c9854db0ef5af37317c1eb42fc4680
shindeal <97160971+shindeal@users.noreply.github.com>
2024-07-24 12:23:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
 import streamlit as st
 import pyodbc
-import pandas as pd
 
 # Initialize connection.
+# Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    connection_string = (
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=" + st.secrets["server"] + ";"
-        "DATABASE=" + st.secrets["database"] + ";"
-        "Trusted_Connection=yes;"  # Use this for Windows Authentication
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
     )
-    return pyodbc.connect(connection_string)
 
 conn = init_connection()
 
 # Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
-        columns = [column[0] for column in cur.description]
-        data = cur.fetchall()
-        return columns, data
+        return cur.fetchall()
 
-query = "SELECT TOP 10 * FROM dim_depot with (nolock);"
-
-# Retrieve data from the database
-columns, rows = run_query(query)
+rows = run_query("SELECT top 10 * from dim_depot with (nolock);")
 
 # Print results.
-st.write("### Query Results")
-df = pd.DataFrame(rows, columns=columns)
-st.write(df)
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
