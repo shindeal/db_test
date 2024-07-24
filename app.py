a63037f4fab9c00bd05bd03a318ab9d31af236a1
shindeal <97160971+shindeal@users.noreply.github.com>
2024-07-24 14:57:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def get_connection():
         + st.secrets["username"]
         + ";PWD="
         + st.secrets["password"]
+        + ";Connection Timeout=30;"  # Increase timeout to 30 seconds
     )
 
 
